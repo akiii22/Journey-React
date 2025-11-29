d96e780e80ca87d9cca279a7c45d140ad7afab7f
jeromee <bulosanjerome50@gmail.com>
2025-11-29 10:05:16 +0800

    learning in typescript how the object as a parameter in function

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -244,3 +244,66 @@ function processInput(random: string | number): void {
 
 processInput("jerome");
 processInput(12);
+
+//** ## Functions - Using Objects as Function Parameters
+
+function createEmployee({ id }: { id: number }): {
+  id: number;
+  isActive: boolean;
+} {
+  return { id: 24, isActive: id % 2 === 0 };
+}
+
+const first = createEmployee({ id: 1 });
+const second = createEmployee({ id: 2 });
+console.log(first, second);
+
+// alternative
+
+function createStudent(student: { id: number; name: string }): void {
+  console.log(`Welcome ${student.name} with an id of ${student.id} in Class B`);
+}
+
+const newStudent = createStudent({ id: 23, name: "Anna" });
+
+/*
+TypeScript only performs excess property checks on object literals where they're used, not on references to them.
+
+In TypeScript, when you pass an object literal (like { id: 1, name: 'bob', email: 'bob@gmail.com' }) directly to a function or assign it to a variable with a specified type, TypeScript checks that the object only contains known properties. This is done to catch common errors.
+
+However, when you pass newStudent to createStudent, TypeScript doesn't complain about the email property. This is because newStudent is not an object literal at the point where it's passed to createStudent.
+
+*/
+
+//! Challenge
+
+/* Your task is to create a function named processData that accepts two parameters:
+
+- The first parameter, input, should be a union type that can be either a string or a number.
+- The second parameter, config, should be an object with a reverse property of type boolean, by default it "reverse" should be false
+
+The function should behave as follows:
+
+- If input is of type number, the function should return the square of the number.
+- If input is of type string, the function should return the string in uppercase.
+- If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
+
+*/
+
+function processData(
+  input: number | string,
+  config: { reverse: boolean } = { reverse: false }
+): string | number {
+  if (typeof input === "number") {
+    return input ** 2;
+  } else {
+    return config.reverse
+      ? input.toUpperCase().split("").reverse().join("")
+      : input.toUpperCase();
+  }
+}
+
+const data1 = processData(22, { reverse: true });
+const data2 = processData("jerome");
+const data3 = processData("bulosan", { reverse: true });
+console.log(data1, data2, data3);
