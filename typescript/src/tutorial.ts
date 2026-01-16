83fb90a3704ff8a6277b0b9f70f475de181276a9
jeromee <bulosanjerome50@gmail.com>
2026-01-16 10:10:09 +0800

    Learning type enum, tuples, assertion, unkown and never

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -629,3 +629,232 @@ A type alias is a way to give a name to a type. It can represent primitive types
 Key Differences
 - Type aliases can represent primitive types, union types, intersection types, tuples, etc., while interfaces are primarily used to represent the shape of an object.
 */
+
+/*
+## Tuples
+In TypeScript, a Tuple is a special type that allows you to create an array where the type of a fixed number of elements is known, but need not be the same - in other words it's an array with fixed length and ordered with fixed types. This is useful when you want to group different types of values together.
+
+Tuples are useful when you want to return multiple values from a function.
+
+By default, tuples in TypeScript are not read-only. This means you can modify the values of the elements in the tuple.However, TypeScript does provide a way to make tuples read-only using the readonly keyword.
+
+*/
+
+// let person: [string, number] = ["john", 25];
+
+// function getPerson(): [string, number] {
+//   return ["john", 25];
+// }
+
+// let randomPerson = getPerson();
+// console.log(randomPerson);
+
+/*
+## Enums
+
+Enums in TypeScript allow us to define a set of named constants. Using enums can make it easier to document intent, or create a set of distinct cases.
+
+*/
+
+// enum ServerResponseStatus {
+//   Success = 200,
+//   Error = 500,
+// }
+
+// Object.values(ServerResponseStatus).forEach((value) => {
+//   if (typeof value === "number") {
+//     console.log(value);
+//   }
+// });
+
+// console.log(ServerResponseStatus);
+
+// interface Serverresponse {
+//   result: ServerResponseStatus;
+//   data: string[];
+// }
+
+// function getServerResponse(): Serverresponse {
+//   return {
+//     result: ServerResponseStatus.Success,
+//     data: ["john", "belga"],
+//   };
+// }
+// const response: Serverresponse = getServerResponse();
+// console.log(response);
+
+/*
+## Enums - Gotcha : Reverse Mapping
+
+In a numeric enum, TypeScript creates a reverse mapping from the numeric values to the enum member names. This means that if you assign a numeric value to an enum member, you can use that numeric value anywhere the enum type is expected.
+
+In a string enum, TypeScript does not create a reverse mapping. This means that if you assign a string value to an enum member, you cannot use that string value anywhere the enum type is expected. You must use the enum member itself.
+*/
+
+/*
+## Challenge
+
+- Define an enum named UserRole with members Admin, Manager, and Employee.
+- Define a type alias named User with properties id (number), name (string), role (UserRole), and contact (a tuple with two elements: email as string and phone as string).
+- Define a function named createUser that takes a User object as its parameter and returns a User object.
+- Call the createUser function with an object that matches the User type, store the result in a variable, and log the variable to the console.
+
+*/
+
+// enum UserRole {
+//   Admin = "admin",
+//   Manager = "manager",
+//   Employee = "employee",
+// }
+
+// type User = {
+//   id: number;
+//   name: string;
+//   role: UserRole;
+//   contact: [email: string, phone: string];
+// };
+
+// function createUser(user: User): User {
+//   return user;
+// }
+
+// const user1: User = {
+//   id: 1,
+//   name: "john",
+//   role: UserRole.Employee,
+//   contact: ["john@gmail.com", "123-456"],
+// };
+
+// console.log(createUser(user1));
+
+/*
+## Type Assertion
+
+Type assertion in TypeScript is a way to tell the compiler what the type of an existing variable is. This is especially useful when you know more about the type of a variable than TypeScript does.
+
+*/
+
+// let someValue: any = "this is a string";
+// let strLength: number = (someValue as string).length;
+
+// type Bird = {
+//   name: string;
+// };
+
+// let birdString = '{ "name": "Eagle" }';
+// let dogString = '{ "breed": "Poodle" }';
+
+// let birdObject = JSON.parse(birdString);
+// let dogObject = JSON.parse(dogString);
+
+// let bed = birdObject as Bird;
+// let pulogoso = dogObject as Bird;
+
+// console.log(bed.name);
+// console.log(pulogoso.name);
+
+// enum Status {
+//   Pending = "pending",
+//   Decline = "declined",
+// }
+
+// type User = {
+//   name: string;
+//   status: Status;
+// };
+
+// save Status.Pending in the DB as a string
+// retrieve string from the DB
+
+// const statusValue = "pending";
+
+// const user: User = { name: "John", status: statusValue as Status };
+
+/*
+## Type - 'unknown'
+
+The unknown type in TypeScript is a type-safe counterpart of the any type. It's like saying that a variable could be anything, but we need to perform some type-checking before we can use it.
+
+"error instanceof Error" checks if the error object is an instance of the Error class.
+
+*/
+
+// let unknownValue: unknown;
+
+// unknownValue = "Hello World";
+// unknownValue = [12, "jerome"];
+// unknownValue = 43.23556;
+
+// if (typeof unknownValue === "number") {
+//   unknownValue.toFixed(2);
+// }
+
+// function runSomeCode() {
+//   const random = Math.random();
+//   if (random < 0.5) {
+//     throw new Error("there was an error");
+//   } else {
+//     throw "some error";
+//   }
+// }
+
+// try {
+//   runSomeCode();
+// } catch (error) {
+//   if (error instanceof Error) {
+//     console.log(error.message);
+//   } else {
+//     console.log(error);
+//   }
+// }
+
+/*
+## Type - "never"
+
+In TypeScript, never is a type that represents the type of values that never occur.you can't assign any value to a variable of type never.
+TypeScript will give a compile error if there are any unhandled cases, helping ensure that all cases are handled.
+
+*/
+
+// let someValue: never = 0;
+
+type Theme = "light" | "dark";
+
+function checkTheme(theme: Theme): void {
+  if (theme === "light") {
+    console.log("light theme");
+    return;
+  }
+  if (theme === "dark") {
+    console.log("dark theme");
+    return;
+  }
+  theme;
+}
+
+enum Color {
+  Red,
+  Blue,
+  Green,
+}
+
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return "Red";
+    case Color.Blue:
+      return "Blue";
+
+    case Color.Green:
+      return "Green";
+    default:
+      //at build time
+      let unexpected: never = color;
+      //at run time
+      throw new Error(`Unexpected color value: ${unexpected}`);
+  }
+}
+
+console.log(getColorName(Color.Red));
+console.log(getColorName(Color.Blue));
+console.log(getColorName(Color.Green));
